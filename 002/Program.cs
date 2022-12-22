48c84fa881b25976ce6528da203088514245c9e8
Deinail <cssrudom@gmail.com>
2022-12-22 16:17:47 +0500

    Дз задание 2

---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+System.Console.WriteLine();
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a<b)
+    System.Console.WriteLine("Число " + b + " больше числа " + a);
+
+else 
+    if (a<b)
+    System.Console.WriteLine("Число " + a + " больше числа " + b );
