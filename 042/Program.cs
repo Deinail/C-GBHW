802da4b775f33cee0feddde2582b717d0eb17703
Deinail <cssrudom@gmail.com>
2023-01-23 08:14:58 +0500

    Задания 42,43

---
+++ 042/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+System.Console.WriteLine();
+
+Console.WriteLine("");
+System.Console.WriteLine("Введите количество чисел");
+int ArrLength = Convert.ToInt32(Console.ReadLine());
+int [] Array = new int [ArrLength];
+int Counter = 0;
+
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.WriteLine("Введите " + (i+1) + " число");
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] < 0)
+        Counter++;
+}
+
+System.Console.WriteLine($"[{string.Join(", ",  Array)}]"); 
+System.Console.WriteLine("Количество чисел больше 0: " + Counter);
---
+++ 043/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b1 - b2) / (k2 - k1);
+double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+
+if (k1 == k2) Console.Write("Заданные прямые не пересекаются!");
+    else
+        Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
