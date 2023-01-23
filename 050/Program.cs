b05a9fa3dda81bf5b9db70432950801e34c45920
Deinail <cssrudom@gmail.com>
2023-01-23 15:25:55 +0500

    Задания 47, 50, 52

---
+++ 050/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void InputMatrix (double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (100 - 1) + 1, 2); // [1, 100]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+Console.Clear();
+double[,] matrix = new double [new Random().Next(1,10), new Random().Next(1,10)];
+InputMatrix(matrix);
+
+System.Console.Write("Введите позицию элемента через пробел: ");
+int[] Position = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+if (matrix.GetLength(0) > Position[0] && matrix.GetLength(1) > Position[1])
+{
+    Console.Write($"{matrix[Position[0], Position[1]]}");
+}
+
+else
+{
+    System.Console.WriteLine("такого элемента нет");
+}
+
---
+++ 052/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+void InputMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int[,] matrix = new int [new Random().Next(4,6), new Random().Next(4,6)];
+InputMatrix(matrix);
+
+Console.WriteLine("---------------------------");
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{ sum / matrix.GetLength(0)} \t");
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Количество столбцов " + matrix.GetLength(1));
+Console.WriteLine();
