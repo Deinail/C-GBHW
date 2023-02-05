864fbf86acf120c492882eea479ef20e236759fa
Ilya Derbenev <66101086+Deinail@users.noreply.github.com>
2023-02-05 09:26:59 +0500

    lession 8,9

---
+++ 054/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int[,] matrix = new int[new Random().Next(4, 6), new Random().Next(4, 6)];
+InputMatrix(matrix);
+
+Console.WriteLine("---------------------------");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            if (matrix[i, k] < matrix[i, k + 1])
+            {
+                int temp = matrix[i, k + 1];
+                matrix[i, k + 1] = matrix[i, k];
+                matrix[i, k] = temp;
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
